d438097a52e76c10d401c64173794bf554ff67f0
Marcondes Cunha Araujo <clavem303@google.com>
2025-06-22 13:30:54 -0300

    Criado a classe WriteBoard.kt e a activity PhrasesActivity.kt.

---
+++ app/src/main/java/com/clavem303/activities/classes/WriteBoard.kt
@@ -0,0 +1,10 @@
+package com.clavem303.activities.classes
+
+class WriteBoard {
+    val phrases = mutableListOf(
+        "A mutableListOf<String> é uma função do Kotlin que cria uma lista mutável capaz de armazenar" +
+                " elementos do tipo String.",
+        "Activity é uma classe abstrata que representa uma atividade ou tela na interface do usuário.",
+        "Activity é a combinação da classe MainActivity e do arquivo activity_main.xml.",
+    )
+}
---
+++ app/src/main/java/com/clavem303/activities/screens/PhrasesActivity.kt
@@ -0,0 +1,21 @@
+package com.clavem303.activities.screens
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.clavem303.activities.R
+
+class PhrasesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_phrases)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
